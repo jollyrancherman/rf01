f104288160e5e7f276f877f1be7954463f3e2583
Anthony <anthony.rsc@gmail.com>
2022-03-13 16:37:28 -0700

    Updates to the robot list

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import "tachyons";
+import Cardlist from "./CardList";
+
+import { robots } from "./robots";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Cardlist robots={robots} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
